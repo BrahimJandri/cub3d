277f230f4c203909cbb76d5bf099c53bba3ec6c0
bjandri <bzaza066@gmail.com>
2024-10-10 12:28:16 +0100

    leeeeeeeet's gooo

--- header.h
+++ header.h
@@ -6,18 +6,20 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 20:46:48 by bjandri           #+#    #+#             */
-/*   Updated: 2024/09/24 21:04:24 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/10/10 11:24:41 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
 
-# include "./minilibx-linux/mlx.h"
+# include "./minilibx/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include "Libft/libft.h"
+# include <signal.h>
+# include <fcntl.h>
 
 int check_path(char *str);
 
