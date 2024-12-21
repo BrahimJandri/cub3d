fc54d01ba2fe20ff4891e13c68f2a22058fccdae
BrahimJandri <bzaza066@gmail.com>
2024-12-21 15:32:46 +0100

    still working on parsing

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reddamss <damssi.art@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 13:05:12 by reddamss          #+#    #+#             */
+/*   Updated: 2023/11/30 10:20:35 by reddamss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n - 1 && str1[i] == str2[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
