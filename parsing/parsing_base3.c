8b03af6301bb175fc7a168f2ba4829ad19c6bea3
BrahimJandri <bzaza066@gmail.com>
2024-12-26 15:19:34 +0100

    came across to finish parsing

--- parsing/parsing_base3.c
+++ parsing/parsing_base3.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 12:45:08 by bjandri           #+#    #+#             */
-/*   Updated: 2024/12/26 15:15:37 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/12/26 15:17:33 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,106 +87,45 @@ void check_map_columns(t_game *game)
 	}
 }
 
-// void check_map_boundaries(t_game *game)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	while (game->map[i])
-// 	{
-// 		j = 0;
-// 		while (game->map[i][j] && (game->map[i][j] == ' ' || game->map[i][j] == '\t'))
-// 			j++;
-// 		if (game->map[i][j] == '\0' || game->map[i][j] == '\n')
-// 		{
-// 			i++;
-// 			continue;
-// 		}
-// 		if (i == 0)
-// 		{
-// 			while (game->map[i][j] && game->map[i][j] != '\n')
-// 			{
-// 				if (game->map[i][j] != '1' && game->map[i][j] != ' ' && game->map[i][j] != '\t')
-// 					error_msg("Error\nMap Not Surrounded by Walls");
-// 				j++;
-// 			}
-// 		}
-// 		else if (game->map[i + 1] == NULL)
-// 		{
-// 			while (game->map[i][j] && game->map[i][j] != '\n')
-// 			{
-// 				if (game->map[i][j] != '1' && game->map[i][j] != ' ')
-// 					error_msg("Error\nMap Not Surrounded by Walls");
-// 				j++;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			if (game->map[i][j] != '1')
-// 				error_msg("Error\nMap Not Surrounded by Walls in Middle Line 1");
-// 			while (game->map[i][j] && game->map[i][j] != '\n')
-// 				j++;
-// 			if (game->map[i][j - 1] != '1' && game->map[i][j - 1] != ' ' && game->map[i][j - 1] != '\t')
-// 				error_msg("Error\nMap Not Surrounded by Walls in Middle Line 2");
-// 		}
-// 		i++;
-// 	}
-// }
-
 void check_initial_final_lines(t_game *game, int i)
 {
-	int j = 0;
+	int j;
 
+	j = 0;
 	while (game->map[i][j] && game->map[i][j] != '\n')
 	{
 		if (game->map[i][j] != '1' && game->map[i][j] != ' ' && game->map[i][j] != '\t')
-		{
 			error_msg("Error\nMap Not Surrounded by Walls");
-		}
 		j++;
 	}
 }
 
 void check_middle_lines(t_game *game, int i)
 {
-	int j = 0;
+	int j;
 
+	j = 0;
 	while (game->map[i][j] && (game->map[i][j] == ' ' || game->map[i][j] == '\t'))
-	{
 		j++;
-	}
-
 	if (game->map[i][j] != '1')
-	{
 		error_msg("Error\nMap Not Surrounded by Walls in Middle Line 1");
-	}
-
 	while (game->map[i][j] && game->map[i][j] != '\n')
-	{
 		j++;
-	}
-
 	if (game->map[i][j - 1] != '1' && game->map[i][j - 1] != ' ' && game->map[i][j - 1] != '\t')
-	{
 		error_msg("Error\nMap Not Surrounded by Walls in Middle Line 2");
-	}
 }
 
 void check_map_boundaries(t_game *game)
 {
-	int i = 0;
-
+	int i;
+	
+	i = 0;
 	while (game->map[i])
 	{
 		if (i == 0 || game->map[i + 1] == NULL)
-		{
 			check_initial_final_lines(game, i);
-		}
 		else
-		{
 			check_middle_lines(game, i);
-		}
 		i++;
 	}
 }
