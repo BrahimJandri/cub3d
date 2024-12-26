7440af1a6e5a0b66e852f99d25a690dd0a6d4259
BrahimJandri <bzaza066@gmail.com>
2024-12-26 15:08:00 +0100

    came across to finish parsing

--- parsing/parsing_base2.c
+++ parsing/parsing_base2.c
@@ -6,15 +6,41 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 12:43:31 by bjandri           #+#    #+#             */
-/*   Updated: 2024/12/24 13:20:42 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/12/26 14:59:50 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Headers/cub3d.h"
 
-char	*parse_textures_and_colors(t_game *game, char *line, int fd)
+char *parse_texture_line(t_game *game, char *line)
 {
-	char	*trimmed_line;
+	if (ft_strncmp(line, "NO ", 3) == 0)
+		game->no_texture = parse_texture(line, &game->no_texture, game);
+	else if (ft_strncmp(line, "SO ", 3) == 0)
+		game->so_texture = parse_texture(line, &game->so_texture, game);
+	else if (ft_strncmp(line, "WE ", 3) == 0)
+		game->we_texture = parse_texture(line, &game->we_texture, game);
+	else if (ft_strncmp(line, "EA ", 3) == 0)
+		game->ea_texture = parse_texture(line, &game->ea_texture, game);
+	else
+		return NULL;
+	return line;
+}
+
+char *parse_color_line(t_game *game, char *line)
+{
+	if (ft_strncmp(line, "F ", 2) == 0)
+		game->floor_color = parse_color(line + 2, game);
+	else if (ft_strncmp(line, "C ", 2) == 0)
+		game->ceiling_color = parse_color(line + 2, game);
+	else
+		return NULL;
+	return line;
+}
+
+char *parse_textures_and_colors(t_game *game, char *line, int fd)
+{
+	char *trimmed_line;
 
 	while (line)
 	{
@@ -24,33 +50,24 @@ char	*parse_textures_and_colors(t_game *game, char *line, int fd)
 			free(line);
 			free(trimmed_line);
 			line = get_next_line(fd);
-			continue ;
+			continue;
 		}
 		free(trimmed_line);
-		if (ft_strncmp(line, "NO ", 3) == 0)
-			game->no_texture = parse_texture(line, &game->no_texture, game);
-		else if (ft_strncmp(line, "SO ", 3) == 0)
-			game->so_texture = parse_texture(line, &game->so_texture, game);
-		else if (ft_strncmp(line, "WE ", 3) == 0)
-			game->we_texture = parse_texture(line, &game->we_texture, game);
-		else if (ft_strncmp(line, "EA ", 3) == 0)
-			game->ea_texture = parse_texture(line, &game->ea_texture, game);
-		else if (ft_strncmp(line, "F ", 2) == 0)
-			game->floor_color = parse_color(line + 2, game);
-		else if (ft_strncmp(line, "C ", 2) == 0)
-			game->ceiling_color = parse_color(line + 2, game);
-		else
-			break ;
+		if (parse_texture_line(game, line) == NULL)
+		{
+			if (parse_color_line(game, line) == NULL)
+				break;
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
-	return (line);
+	return line;
 }
 
-char	*skip_empty_lines(int fd)
+char *skip_empty_lines(int fd)
 {
-	char	*line;
-	char	*trimmed_line;
+	char *line;
+	char *trimmed_line;
 
 	line = get_next_line(fd);
 	if (line == NULL)
@@ -76,12 +93,12 @@ char	*skip_empty_lines(int fd)
 	}
 }
 
-void	calculate_map_dimensions(t_game *game, const char *file)
+void calculate_map_dimensions(t_game *game, const char *file)
 {
-	int		fd;
-	char	*line;
-	char	*trimed_line;
-	int		line_length;
+	int fd;
+	char *line;
+	char *trimed_line;
+	int line_length;
 
 	fd = open_file(file);
 	line = skip_empty_lines(fd);
@@ -100,41 +117,52 @@ void	calculate_map_dimensions(t_game *game, const char *file)
 	close(fd);
 }
 
-void	fill_map(t_game *game, const char *file)
+char *read_and_process_line(int fd)
 {
-	int		fd;
-	int		i;
-	int		line_length;
-	char	*padded_line;
-	char	*line;
+	char *line;
+
+	line = skip_empty_lines(fd);
+	return skip_texture_colors(fd, line);
+}
+
+char *pad_line(const char *line, int map_width)
+{
+	int line_length = ft_strlen(line);
+	char *padded_line;
+
+	if (line_length < map_width)
+	{
+		padded_line = malloc(map_width + 1);
+		if (!padded_line)
+			error_msg("Error\nMemory allocation error");
+		ft_memcpy(padded_line, line, line_length);
+		ft_memset(padded_line + line_length, ' ', map_width - line_length);
+		padded_line[map_width] = '\0';
+	}
+	else
+	{
+		padded_line = ft_strdup(line);
+	}
+	return padded_line;
+}
+
+void fill_map(t_game *game, const char *file)
+{
+	int fd;
+	int i = 0;
+	char *line;
 
 	fd = open_file(file);
-	i = 0;
-	line = skip_empty_lines(fd);
-	line = skip_texture_colors(fd, line);
+	line = read_and_process_line(fd);
 	game->map = malloc(sizeof(char *) * (game->map_height + 1));
 	if (!game->map)
 		error_msg("Error\nMemory allocation error");
+
 	while (line)
 	{
 		if (*line != '\0')
 		{
-			line_length = ft_strlen(line);
-			if (line_length < game->map_width)
-			{
-				padded_line = malloc(game->map_width + 1);
-				if (!padded_line)
-					error_msg("Error\nMemory allocation error");
-				ft_memcpy(padded_line, line, line_length);
-				ft_memset(padded_line + line_length, ' ', game->map_width
-					- line_length);
-				padded_line[game->map_width] = '\0';
-				game->map[i] = padded_line;
-			}
-			else
-			{
-				game->map[i] = ft_strdup(line);
-			}
+			game->map[i] = pad_line(line, game->map_width);
 			i++;
 		}
 		free(line);
--- parsing/parsing_base3.c
+++ parsing/parsing_base3.c
@@ -6,17 +6,17 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 12:45:08 by bjandri           #+#    #+#             */
-/*   Updated: 2024/12/25 15:31:09 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/12/26 15:07:43 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Headers/cub3d.h"
 
-void	count_params(t_game *game)
+void count_params(t_game *game)
 {
-	int	i;
-	int	j;
-	int	count;
+	int i;
+	int j;
+	int count;
 
 	i = 0;
 	count = 0;
@@ -25,8 +25,7 @@ void	count_params(t_game *game)
 		j = 0;
 		while (j < game->map_width)
 		{
-			if (game->map[i][j] == 'N' || game->map[i][j] == 'S'
-				|| game->map[i][j] == 'E' || game->map[i][j] == 'W')
+			if (game->map[i][j] == 'N' || game->map[i][j] == 'S' || game->map[i][j] == 'E' || game->map[i][j] == 'W')
 			{
 				game->player_x = i;
 				game->player_y = j;
@@ -40,10 +39,10 @@ void	count_params(t_game *game)
 		error_msg("Error\nInvalid params of the map");
 }
 
-void	check_map_params(t_game *game)
+void check_map_params(t_game *game)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	while (i < game->map_height)
@@ -51,10 +50,7 @@ void	check_map_params(t_game *game)
 		j = 0;
 		while (game->map[i][j])
 		{
-			if ((game->map[i][j] != '1' && game->map[i][j] != '0')
-				&& (game->map[i][j] != 'N' && game->map[i][j] != 'S'
-					&& game->map[i][j] != 'E' && game->map[i][j] != 'W')
-				&& game->map[i][j] != 32 && game->map[i][j] != '\n')
+			if ((game->map[i][j] != '1' && game->map[i][j] != '0') && (game->map[i][j] != 'N' && game->map[i][j] != 'S' && game->map[i][j] != 'E' && game->map[i][j] != 'W') && game->map[i][j] != 32 && game->map[i][j] != '\n')
 				error_msg("Error\nBad Params on map.");
 			j++;
 		}
@@ -62,64 +58,56 @@ void	check_map_params(t_game *game)
 	}
 }
 
-void	check_config(t_game *game)
+void check_config(t_game *game)
 {
 	if (game->config_count != 6)
 		error_msg("Error\nTextures or Colors are not correct.");
 }
 
-void	check_map_columns(t_game *game)
+void check_map_columns(t_game *game)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	j = 0;
 	while (j < game->map_width)
 	{
 		i = 0;
-		while (game->map[i] && (j >= (int)ft_strlen(game->map[i])
-				|| game->map[i][j] == ' ' || game->map[i][j] == '\t'
-				|| game->map[i][j] == '\n' || game->map[i][j] == '\0'))
+		while (game->map[i] && (j >= (int)ft_strlen(game->map[i]) || game->map[i][j] == ' ' || game->map[i][j] == '\t' || game->map[i][j] == '\n' || game->map[i][j] == '\0'))
 			i++;
-		if (game->map[i] && j < (int)ft_strlen(game->map[i])
-			&& game->map[i][j] != '1')
+		if (game->map[i] && j < (int)ft_strlen(game->map[i]) && game->map[i][j] != '1')
 			error_msg("Error\nMap Not Surrounded by Walls at Left Boundary");
-		while (game->map[i] && j < (int)ft_strlen(game->map[i])
-			&& game->map[i][j] != '\n')
+		while (game->map[i] && j < (int)ft_strlen(game->map[i]) && game->map[i][j] != '\n')
 			i++;
-		while (i > 0 && (j >= (int)ft_strlen(game->map[i - 1]) || game->map[i
-				- 1][j] == ' ' || game->map[i - 1][j] == '\t' || game->map[i
-				- 1][j] == '\n' || game->map[i - 1][j] == '\0'))
+		while (i > 0 && (j >= (int)ft_strlen(game->map[i - 1]) || game->map[i - 1][j] == ' ' || game->map[i - 1][j] == '\t' || game->map[i - 1][j] == '\n' || game->map[i - 1][j] == '\0'))
 			i--;
-		if (i > 0 && j < (int)ft_strlen(game->map[i - 1]) && game->map[i
-			- 1][j] != '1')
+		if (i > 0 && j < (int)ft_strlen(game->map[i - 1]) && game->map[i - 1][j] != '1')
 			error_msg("Error\nMap Not Surrounded by Walls at Right Boundary");
 		j++;
 	}
 }
 
-void	check_map_boundaries(t_game *game)
+void check_map_boundaries(t_game *game)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	while (game->map[i])
 	{
 		j = 0;
-		while (game->map[i][j] && (game->map[i][j] == ' '
-				|| game->map[i][j] == '\t'))
+		while (game->map[i][j] && (game->map[i][j] == ' ' || game->map[i][j] == '\t'))
 			j++;
 		if (game->map[i][j] == '\0' || game->map[i][j] == '\n')
 		{
 			i++;
-			continue ;
+			continue;
 		}
 		if (i == 0)
 		{
 			while (game->map[i][j] && game->map[i][j] != '\n')
 			{
-				if (game->map[i][j] != '1' && game->map[i][j] != ' ' && game->map[i][j] != '\t') 
+				if (game->map[i][j] != '1' && game->map[i][j] != ' ' && game->map[i][j] != '\t')
 					error_msg("Error\nMap Not Surrounded by Walls");
 				j++;
 			}
@@ -137,7 +125,7 @@ void	check_map_boundaries(t_game *game)
 		{
 			if (game->map[i][j] != '1')
 				error_msg("Error\nMap Not Surrounded by Walls in Middle Line 1");
-			while (game->map[i][j] && game->map[i][j] != '\n') 
+			while (game->map[i][j] && game->map[i][j] != '\n')
 				j++;
 			if (game->map[i][j - 1] != '1' && game->map[i][j - 1] != ' ' && game->map[i][j - 1] != '\t')
 				error_msg("Error\nMap Not Surrounded by Walls in Middle Line 2");
