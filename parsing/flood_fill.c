8286c25ca38f77c0fe375a19da1548a0f90b6af6
BrahimJandri <bzaza066@gmail.com>
2024-12-29 11:29:09 +0100

    came a cross to fnish parsing incha2llah

--- parsing/flood_fill.c
+++ parsing/flood_fill.c
@@ -6,15 +6,15 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 12:52:43 by bjandri           #+#    #+#             */
-/*   Updated: 2024/12/27 09:06:10 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/12/29 11:27:14 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Headers/cub3d.h"
 
-void	map_dup(t_game *game)
+void map_dup(t_game *game)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	game->map_dup = malloc(sizeof(char *) * (game->map_height + 1));
@@ -26,22 +26,18 @@ void	map_dup(t_game *game)
 	game->map_dup[i] = NULL;
 }
 
-bool	ft_flood_fill(int x, int y, t_game *game)
+bool ft_flood_fill(int x, int y, t_game *game)
 {
-	bool	up;
-	bool	down;
-	bool	left;
-	bool	right;
+	bool up;
+	bool down;
+	bool left;
+	bool right;
 
 	if (x < 0 || y < 0 || x >= game->map_height || y >= game->map_width)
 		return (false);
-	if (game->map_dup[x][y] == 'V' || game->map_dup[x][y] == '1')
+	if (game->map_dup[x][y] == 'V' || game->map[x][y] == '1')
 		return (true);
-	if ((game->map[x][y] == '0' && (x > 0 && game->map[x - 1][y] == ' '))
-		|| (game->map[x][y] == '0' && (x < game->map_height - 1 && game->map[x
-				+ 1][y] == ' ')) || (game->map[x][y] == '0' && (y > 0
-				&& game->map[x][y - 1] == ' ')) || (game->map[x][y] == '0'
-			&& (y < game->map_width - 1 && game->map[x][y + 1] == ' ')))
+	if (game->map[x][y] == ' ')
 		return (false);
 	game->map_dup[x][y] = 'V';
 	up = ft_flood_fill(x - 1, y, game);
