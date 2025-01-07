d01c6b56b2b75790361386a9b6b95c9ee46964a7
bjandri <bjandri@Debian.myguest.virtualbox.org>
2025-01-07 15:16:29 +0100

    l3a9l ino rayba9i

--- parsing/check_boundaries.c
+++ parsing/check_boundaries.c
@@ -37,11 +37,11 @@ void check_last_line(t_game *game, int *j, int *i)
 
 void check_middle_line(t_game *game, int *i, int *j)
 {
-	if (game->map[*i][*j] != '1')
+	if (game->map[*i][*j] != '1' && game->map[*i][*j] != ' ')
 		error_msg("Error\nMap Not Surrounded by Walls in Middle Line");
 	while (game->map[*i][*j] && game->map[*i][*j] != '\n')
 		(*j)++;
-	if (game->map[*i][*j - 1] != '1')
+	if (game->map[*i][*j - 1] != '1' && game->map[*i][*j - 1] != ' ')
 		error_msg("Error\nMap Not Surrounded by Walls in Middle Line");
 }
 
@@ -59,13 +59,13 @@ void check_map_boundaries(t_game *game)
 		if (game->map[i][j] == '\0' || game->map[i][j] == '\n')
 		{
 			i++;
-			continue; // Skip empty or whitespace-only lines
+			continue;
 		}
 		if (i == 0)
 			check_first_line(game, &j);
-		else if (game->map[i + 1] == NULL) // Check the last line
+		else if (game->map[i + 1] == NULL)
 			check_last_line(game, &j, &i);
-		else // Check middle lines
+		else
 			check_middle_line(game, &i, &j);
 		i++;
 	}
