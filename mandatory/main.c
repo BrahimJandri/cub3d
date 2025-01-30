73a35334660604c7792a3f9a75cc7a7b9f3d669b
BrahimJandri <bzaza066@gmail.com>
2025-01-30 15:43:49 +0100

    work on leaks

--- mandatory/main.c
+++ mandatory/main.c
@@ -160,8 +160,8 @@ int main(int ac, char **av)
     // t_image image;
     (void)ac;
 
-    init_player(game);//init dakchi d lplayer kamlo hna
     parse_config(game, av);
+    init_player(game);//init dakchi d lplayer kamlo hna
     init_ray(game);
 
     game->mlx = mlx_init();
