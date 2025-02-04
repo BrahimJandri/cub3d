8605753840b0b75b05946e1e98f53ac6d694051d
BrahimJandri <bzaza066@gmail.com>
2025-02-04 10:51:42 +0100

    minmap norm

--- bonus/minimap_bonus.c
+++ bonus/minimap_bonus.c
@@ -12,47 +12,70 @@
 
 #include "./Headers/cub3d_bonus.h"
 
-void draw_tile_within_bounds(t_game *data, int map_x, int map_y)
+void	draw_tile_within_bounds(t_game *data, int map_x, int map_y)
 {
-    if (map_x < 0 || map_y < 0 || map_y >= data->map_height || map_x >= data->map_width)
-        return;
+	char	cell;
+	int		color;
 
-    char cell; 
-    int color;
-    int tile_x;
-    int tile_y;
-
-    cell = data->map[map_y][map_x];
-    color = get_tile_color(cell);
-    if (color == -1)
-        return;
-
-    calculate_tile_position(data, map_x, map_y, &tile_x, &tile_y);
-
-    if (tile_x + (TILE * MINIMAP) > MINIMAP_X && tile_x < MINIMAP_X + MINIMAP_SIZE &&
-        tile_y + (TILE * MINIMAP) > MINIMAP_Y && tile_y < MINIMAP_Y + MINIMAP_SIZE)
-    {
-        draw_tile(data, tile_x, tile_y, color);
-    }
+	if (map_x < 0 || map_y < 0 || map_y >= data->map_height
+		|| map_x >= data->map_width)
+		return ;
+	cell = data->map[map_y][map_x];
+	color = get_tile_color(cell);
+	if (color == -1)
+		return ;
+	calculate_tile_position(data, map_x, map_y);
+	if (data->minimap->tile_x + (TILE * MINIMAP) > MINIMAP_X
+		&& data->minimap->tile_x < MINIMAP_X + MINIMAP_SIZE
+		&& data->minimap->tile_y + (TILE * MINIMAP) > MINIMAP_Y
+		&& data->minimap->tile_y < MINIMAP_Y + MINIMAP_SIZE)
+	{
+		draw_tile(data, color);
+	}
 }
 
-void update_minimap(t_game *data)
+void	update_minimap(t_game *data)
 {
-    static int frame;
-    
-    frame = 0;
-    if (frame % 1 == 0)
-    {
-        draw_minimap(data);
-    }
-    frame++;
+	static int	frame;
+
+	frame = 0;
+	if (frame % 1 == 0)
+	{
+		draw_minimap(data);
+	}
+	frame++;
 }
 
-void draw_minimap(t_game *game)
+void	init_minimap(t_game *game)
 {
-    mlx_clear_window(game->mlx, game->win);
-    draw_background(game);
-    draw_map_mini(game);
-    draw_minimap_player(game);
-    draw_player_direction(game);
+	t_minimap	*minimap;
+
+	minimap = malloc(sizeof(t_minimap));
+	minimap->dx = 0;
+	minimap->dy = 0;
+	minimap->start_x = 0;
+	minimap->start_y = 0;
+	minimap->steps = 0;
+	minimap->xinc = 0;
+	minimap->yinc = 0;
+	minimap->center_x = 0;
+	minimap->center_y = 0;
+	minimap->ray_angle = 0;
+	minimap->ray_length = 0;
+	minimap->visible_range = 0;
+	minimap->player_map_x = 0;
+	minimap->player_map_y = 0;
+	minimap->tile_x = 0;
+	minimap->tile_y = 0;
+	game->minimap = minimap;
+}
+
+void	draw_minimap(t_game *game)
+{
+	init_minimap(game);
+	mlx_clear_window(game->mlx, game->win);
+	draw_background(game);
+	draw_map_mini(game);
+	draw_minimap_player(game);
+	draw_player_direction(game);
 }
